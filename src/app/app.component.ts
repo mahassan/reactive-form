f4e8479b1ff50c0c7c6d1ad02817a34188a51c59
Ammar Hassan <emaarkhan@gmail.com>
2017-11-04 23:46:13 +0000

    Title Changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'lgc-labs-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'lgc-labs';
+  labForm : FormGroup;
+  labs = {
+    Id:'',
+    name : ''
+  }
+
+  ngOnInit(){
+    this.labForm = new FormGroup({
+      'labName': new FormControl(null,[Validators.required,Validators.maxLength(30)])
+    })
+  }
+
+  onSubmit(){
+    console.log(this.labForm);
+    //console.log(this.labs.Id += ;
+    this.labs.name = this.labForm.value.labName;
+    this.labForm.reset();
+  }
+
 }
