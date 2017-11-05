8a6848d59e65803a49e92e957d888334e7afca4f
Ammar Hassan <emaarkhan@gmail.com>
2017-11-05 04:35:29 +0000

    added child component reference

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {LabsDetailsComponent} from './labs-details.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LabsDetailsComponent
   ],
   imports: [
     BrowserModule,
